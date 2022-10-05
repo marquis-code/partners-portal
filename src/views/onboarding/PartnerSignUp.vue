9e9cb0d3de252b7a884ed9b45baa45bd1a510066
Temitope Samson Isaiah <61621188+smit1298@users.noreply.github.com>
2022-10-05 14:57:42 +0100

    resolved comments from the PR

--- src/views/onboarding/PartnerSignUp.vue
+++ src/views/onboarding/PartnerSignUp.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Header from "./components/CenteredPageHeader.vue";
+import Header from "../../components/CenteredPageHeader.vue";
 
 export default defineComponent({
   name: "PartnerSignUp",
