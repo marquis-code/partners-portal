f8a023e44d423488e9d1f3b3e8280ce9f3c21785
tobi.alao <oluwatobi.alao@shuttlers.ng>
2022-10-05 20:07:00 +0100

    Module Augmentation fix

---
+++ src/globals.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import {AxiosInstance} from "axios";
+
+export { }
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+  }
+}
