7cb993b4c807d51ece1d820b646732f51546b069
Daniel Sumah <106399477+dsumah@users.noreply.github.com>
2022-10-07 13:28:53 +0100

    ImageUploadComponent
    Create component for upoading Image..
    
    Added a new item to tailwind.config

---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <form class="" ref="avatar-upload-form">
+    <input
+        type="file"
+        accept="image/*,.pdf"
+        ref="avatar"
+        @change="uploadFile"
+        class="hidden"
+    />
+    </form>
+     <div v-if="!fileUploaded" class="space-y-2 w-full relative">
+          <div
+            class="
+              py-8
+              ring-1 ring-gray-300
+              w-full
+              flex flex-col
+              rounded-md
+              items-center
+              cursor-pointer
+            "
+            @click="$refs.avatar.click()"
+          >
+            <img src="@/assets/images/upload.svg" />
+            <p class="text-sm text-green-400">Click to upload</p>
+            <p class="text-xs text-gray-400">or drag and drop</p>
+          </div>
+        </div>
+        <div
+        v-else
+        class="
+            p-4
+            ring-1 ring-gray-300
+            flex flex-row justify-left
+            rounded-md
+            items-center
+            cursor-pointer
+            w-full md:w-1/2
+            h-24
+            space-x-4
+        "
+        @click="removeFile"
+        >
+        <img src="@/assets/images/export.svg" />
+        <div class="flex flex-col w-full">
+            <div class="doc-info-and-delete flex flex-row justify-between">
+                <div>
+                    <p class="text-sm text-black">{{fileName}}</p>
+                    <p class="text-sm text-gray-400">{{fileSize}} MB</p>
+                </div>
+                <img src="@/assets/images/bin.svg" />
+            </div>
+            <div class="doc-info-and-delete flex flex-row justify-between items-center">
+                <div class="bg-green-500 w-10/12 h-2 rounded-2xl"></div>
+                <p class="w-2/12 text-right">100%</p>
+            </div>
+        </div>
+    </div>
+    <p v-if="fileUploaded" @click="$refs.avatar.click()" class="text-sh-purple-700 font-medium text-sm mb-4">Change Document</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core"
+
+export default defineComponent({
+  name: "ImageUpload",
+  props: {
+    field: String
+  },
+  data () {
+    return {
+      selectedFile: {},
+      fileUploaded: false,
+      fileName: '',
+      fileSize: ''
+    }
+  },
+  methods: {
+    uploadFile(event : any) {
+      console.log(event.target.files[0])
+      if (this.isFileSizeOk(event.target.files[0].size)) {
+        this.selectedFile = event.target.files[0];
+        this.fileSize = (event.target.files[0].size / 1000000).toFixed(1).toString();
+        this.fileUploaded = true;
+        this.fileName = event.target.files[0].name;
+      } else this.$toast.warning('file must be less than 10 MB')
+    },
+    isFileSizeOk(fileSizeInBytes : number) : boolean {
+      if (fileSizeInBytes > 10000000) {
+        return false;
+      } else return true;
+    },
+    removeFile() {
+      this.selectedFile = {};
+      this.fileUploaded = false;
+      this.fileName = '';
+      this.fileSize = '';
+    }
+  },
+});
+</script>
+
+<style scoped>
+</style>
