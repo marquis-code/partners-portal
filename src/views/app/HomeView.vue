6a10dce3a48948c0b796057431dbd989639ec541
Daniel Sumah <106399477+dsumah@users.noreply.github.com>
2022-10-08 19:53:20 +0100

    wip

--- src/views/app/HomeView.vue
+++ src/views/app/HomeView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>Sidebar</div>
-  <div>Navbar
-    <button class="px-4 bg-sh-green-500 text-grays-black-3 text-sm font-medium" @click="logout()">Logout</button>
-  </div>
-  <router-view/>
+  <div class="home bg-gray-100 flex flex-row">
+    <div class="w-64 h-screen py-9 px-4 bg-white">
+      <div class="flex flex-row justify-between mb-9">
+        <img src="@/assets/logo.png" class="h-6">
+        <img src="@/assets/images/toggle.svg">
+      </div>
+      <div class="side-bar-item">
+        <div class="item">
+          <div>
+            <img src="@/assets/images/side-bar.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>Navbar
+      <button class="px-4 bg-sh-green-500 text-grays-black-3 text-sm font-medium" @click="logout()">Logout</button>
+      <router-view/>
+    </div>
+</div>
 </template>
 <script lang="ts">
 import {defineComponent} from "vue";
