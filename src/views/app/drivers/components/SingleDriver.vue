c8b12675c598bbdbab1d6c2a54ed11aaad108233
marquis-code <abahmarquis@gmail.com>
2022-10-14 21:55:41 +0100

    view partners driver UI implementation

--- src/views/app/drivers/components/SingleDriver.vue
+++ src/views/app/drivers/components/SingleDriver.vue
@@ -12,12 +12,21 @@
     <div class="space-y-7 lg:p-8 p-7">
       <p class="text-sm font-medium text-gray-500">Driver Information</p>
       <main class="flex flex-col space-y-2 justify-center items-center">
-        <img v-if="driverData.avatar" :src="driverData.avatar" class="rounded-full h-20 w-20 object-cover" />
-        <span v-else class="rounded-full p-2 text-gray-100 font-medium bg-gray-400">{{ driverData.fname.slice(0, 1)}}{{ driverData.lname.slice(0, 1) }}</span>
+        <img
+          v-if="driverData.avatar"
+          :src="driverData.avatar"
+          class="rounded-full h-20 w-20 object-cover"
+        />
+        <span
+          v-else
+          class="rounded-full h-20 w-20 text-gray-100 font-medium bg-gray-400"
+          >{{ driverData.fname.slice(0, 1)
+          }}{{ driverData.lname.slice(0, 1) }}</span
+        >
       </main>
-        <p class="font-medium text-gray-600 text-center">{{driverData.fname}} {{driverData.lname}}</p>
-      <div>
-      </div>
+      <p class="font-medium text-gray-600 text-center">
+        {{ driverData.fname }} {{ driverData.lname }}
+      </p>
     </div>
     <div class="w-full h-0.5 bg-gray-200"></div>
 
@@ -26,10 +35,14 @@
         <div class="flex justify-start space-x-2">
           <img class="h-5 w-5" src="@/assets/images/carIcon.svg" />
           <div class="">
+            <p class="font-medium text-xs">Assigned vehicle</p>
             <p class="font-medium text-xs">
-              Assigned vehicle
+              {{
+                driverData.assigned_vehicle
+                  ? driverData.assigned_vehicle
+                  : 'N/A'
+              }}
             </p>
-            <p class="font-medium text-xs">{{driverData.assigned_vehicle ? driverData.assigned_vehicle : 'N/A'}}</p>
           </div>
         </div>
         <p class="underline text-indigo-600 text-xs">Assign vehicle</p>
@@ -39,8 +52,10 @@
           <img class="h-5 w-5" src="@/assets/images/plateNumberIcon.svg" />
           <div class="">
             <p class="font-medium text-xs">License number</p>
-            <p class="font-light text-xs">
-              {{driverData.license_number ? driverData.license_number : 'N/A'}}
+            <p class="font-medium text-xs">
+              {{
+                driverData.license_number ? driverData.license_number : 'N/A'
+              }}
             </p>
           </div>
         </div>
@@ -51,21 +66,22 @@
           <img class="h-5 w-5" src="@/assets/images/tripsIcon.svg" />
           <div class="">
             <p class="font-medium text-xs">Total trips taken</p>
-            <p class="font-light text-xs"> {{driverData.total_trips ? driverData.total_trips : 'N/A'}}</p>
+            <p class="font-medium text-xs">
+              {{ driverData.total_trips ? driverData.total_trips : 'N/A' }}
+            </p>
           </div>
         </div>
         <router-link to="trips" class="underline text-indigo-600 text-xs"
           >View all</router-link
         >
       </div>
-    </div>
-    <div class="flex justify-between items-center py-6">
+      <div class="flex justify-between items-center py-6">
         <div class="flex space-x-2">
-          <img class="h-5 w-5" src="@/assets/images/email.svg" />
+          <img class="h-5 w-5" src="@/assets/images/emailIcon.svg" />
           <div class="">
             <p class="font-medium text-xs">Email address</p>
-            <p class="font-light text-xs">
-              {{driverData.email ? driverData.email : 'N/A'}}
+            <p class="font-medium text-xs">
+              {{ driverData.email ? driverData.email : 'N/A' }}
             </p>
           </div>
         </div>
@@ -76,13 +92,14 @@
           <img class="h-5 w-5" src="@/assets/images/phone.svg" />
           <div class="">
             <p class="font-medium text-xs">Phone number</p>
-            <p class="font-light text-xs">
-              {{driverData.phone ? driverData.phone : 'N/A'}}
+            <p class="font-medium text-xs">
+              {{ driverData.phone ? driverData.phone : 'N/A' }}
             </p>
           </div>
         </div>
         <p class="underline text-indigo-600 text-xs">Change phone</p>
       </div>
+    </div>
   </section>
 </template>
 
