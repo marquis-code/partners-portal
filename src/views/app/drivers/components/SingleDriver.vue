56a19f4ce6fc8a4c738689a3510c705511202d62
marquis-code <abahmarquis@gmail.com>
2022-10-14 22:15:29 +0100

    drivers detail page implementation

--- src/views/app/drivers/components/SingleDriver.vue
+++ src/views/app/drivers/components/SingleDriver.vue
@@ -1,6 +1,7 @@
 <template>
   <section
     class="
+      w-full
       lg:w-4/12
       md:w-7/12
       ring-1 ring-gray-50
@@ -24,19 +25,19 @@
           }}{{ driverData.lname.slice(0, 1) }}</span
         >
       </main>
-      <p class="font-medium text-gray-600 text-center">
+      <p class="font-light text-gray-600 text-center">
         {{ driverData.fname }} {{ driverData.lname }}
       </p>
     </div>
     <div class="w-full h-0.5 bg-gray-200"></div>
 
-    <div class="p-8 grid grid-cols-1 divide-y">
+    <div class="p-5 md:p-8 grid grid-cols-1 divide-y">
       <div class="flex justify-between items-center pb-6">
         <div class="flex justify-start space-x-2">
           <img class="h-5 w-5" src="@/assets/images/carIcon.svg" />
           <div class="">
-            <p class="font-medium text-xs">Assigned vehicle</p>
-            <p class="font-medium text-xs">
+            <p class="font-medium text-sm">Assigned vehicle</p>
+            <p class="font-light text-xs">
               {{
                 driverData.assigned_vehicle
                   ? driverData.assigned_vehicle
@@ -51,22 +52,22 @@
         <div class="flex space-x-2">
           <img class="h-5 w-5" src="@/assets/images/plateNumberIcon.svg" />
           <div class="">
-            <p class="font-medium text-xs">License number</p>
-            <p class="font-medium text-xs">
+            <p class="font-medium text-sm">License number</p>
+            <p class="font-light text-xs">
               {{
                 driverData.license_number ? driverData.license_number : 'N/A'
               }}
             </p>
           </div>
         </div>
-        <p class="underline text-indigo-600 text-xs">Change plate number</p>
+        <p class="underline text-indigo-600 text-sm">Change plate number</p>
       </div>
       <div class="flex justify-between items-center py-6">
         <div class="flex space-x-2">
           <img class="h-5 w-5" src="@/assets/images/tripsIcon.svg" />
           <div class="">
             <p class="font-medium text-xs">Total trips taken</p>
-            <p class="font-medium text-xs">
+            <p class="font-light text-xs">
               {{ driverData.total_trips ? driverData.total_trips : 'N/A' }}
             </p>
           </div>
@@ -79,8 +80,8 @@
         <div class="flex space-x-2">
           <img class="h-5 w-5" src="@/assets/images/emailIcon.svg" />
           <div class="">
-            <p class="font-medium text-xs">Email address</p>
-            <p class="font-medium text-xs">
+            <p class="font-medium text-sm">Email address</p>
+            <p class="font-light text-xs">
               {{ driverData.email ? driverData.email : 'N/A' }}
             </p>
           </div>
@@ -91,8 +92,8 @@
         <div class="flex space-x-2">
           <img class="h-5 w-5" src="@/assets/images/phone.svg" />
           <div class="">
-            <p class="font-medium text-xs">Phone number</p>
-            <p class="font-medium text-xs">
+            <p class="font-medium text-sm">Phone number</p>
+            <p class="font-light text-xs">
               {{ driverData.phone ? driverData.phone : 'N/A' }}
             </p>
           </div>
