d240c146ab14b7b91fa299266cf558f9db358492
marquis-code <abahmarquis@gmail.com>
2022-10-18 10:25:48 +0100

    fixed create drivers and vehicles onboarding width

--- src/views/app/drivers/AddDriver.vue
+++ src/views/app/drivers/AddDriver.vue
@@ -13,7 +13,7 @@
         </template>
       </page-action-header>
     </template>
-    <main class="md:w-10/12  mx-auto p-5 lg:p-14 bg-white ring-1 ring-gray-100">
+    <main class="md:w-9/12 p-5 lg:p-14 bg-white ring-1 ring-gray-100">
       <div class="flex justify-center items-center flex-col space-y-2 pb-5">
         <img
           v-if="profilePreview"
