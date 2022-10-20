046ba5878cfdfcf86cfab73991d12448a88c6e78
Daniel Sumah <106399477+dsumah@users.noreply.github.com>
2022-10-20 09:55:34 +0100

    remove unused sections

--- src/views/app/Sidebar.vue
+++ src/views/app/Sidebar.vue
@@ -92,13 +92,13 @@ export default defineComponent({
             {title: 'Vehicles', icon: 'directions_car', selected: false},
             {title: 'Drivers', icon: 'group', selected: false},
             {title: 'Trips', icon: 'route', selected: false},
-            {title: 'Earnings', icon: 'payments', selected: false}
+            // {title: 'Earnings', icon: 'payments', selected: false}
           ],
         },
         {
           sectionTitle: 'SYSTEM',
           menus: [
-            {title: 'Settings', icon: 'settings', selected: false}
+            // {title: 'Settings', icon: 'settings', selected: false}
           ],
         }
       ]
