61563dc1297c332bd2a1bb51db13e7dc5e52da02
Daniel Sumah <106399477+dsumah@users.noreply.github.com>
2022-10-22 04:35:35 +0100

    adding stakeholders

--- src/views/app/Dashboard.vue
+++ src/views/app/Dashboard.vue
@@ -1,26 +1,161 @@
 <template>
   <page-layout page-title="Dashboard">
-    <div class="p-10">
-      <h1 class="">Placeholder for Dashboard Content Area...</h1>
-      <app-modal>Here</app-modal>
+    <div>
+      <div v-if="step === 1" class="h-full p-10 bg-white">
+        <div class="mt-2">
+          <div class="flex flex-row">
+            <img src="@/assets/icons/purple-list.svg" class="mr-3" />
+            <p class="font-medium">Incorporation Certificates</p>
+          </div>
+          <p class="text-sm text-gray-300 pt-5">
+            Proof of existence of business
+          </p>
+          <label class="text-xs font-medium text-grays-black-6"
+            >Upload document</label
+          >
+          <ImageUpload class="pt-3" />
+        </div>
+
+        <div class="mt-2 pt-5">
+          '
+          <div class="flex flex-row">
+            <img src="@/assets/icons/purple-list.svg" class="mr-3" />
+            <p class="font-medium">Incorporation Certificates</p>
+          </div>
+          '
+          <p class="text-sm text-gray-300">Mermorandum of association</p>
+          <label class="text-xs font-medium text-grays-black-5"
+            >UThis holds the information of the first members of your
+            business</label
+          >
+          <image-upload class="pt-3" />
+        </div>
+      </div>
+      <div v-if="step === 2" class="p-10 bg-white h-screen">
+        <div class="flex flex-row">
+          <p class="font-medium">Add all shareholders</p>
+        </div>
+        <div
+          v-for="(stakeholder, index) in stakeholders"
+          :key="index"
+          class="text-sm text-gray-500 pt-3 mb-7"
+        >
+          <section
+            class="
+              lg:flex
+              justify-between
+              space-y-3
+              lg:space-y-0 lg:space-x-10
+              items-center
+            "
+          >
+            <div class="space-y-2 w-full">
+              <label class="text-xs font-medium text-grays-black-5"
+                >Stake Holder's Full Name</label
+              >
+              <input
+                maxlength="11"
+                type="text"
+                class="
+                  text-xs
+                  border-none
+                  outline-none
+                  w-full
+                  rounded-md
+                  p-3
+                  placeholder-gray-500 placeholder-opacity-25
+                  ring-1 ring-gray-300
+                "
+                placeholder="Enter document number"
+              />
+              <!-- <span class="text-sm font-light text-red-500" v-if="v$.identityForm.document.type.$dirty && v$.identityForm.document.type.required.$invalid">
+              This field is required
+            </span> -->
+            </div>
+            <div class="space-y-2 w-full">
+              <label class="text-xs font-medium text-grays-black-5"
+                >Stake Holder Share</label
+              >
+              <input
+                maxlength="11"
+                type="text"
+                class="
+                  text-xs
+                  border-none
+                  outline-none
+                  w-full
+                  rounded-md
+                  p-3
+                  placeholder-gray-500 placeholder-opacity-25
+                  ring-1 ring-gray-300
+                "
+                placeholder="Enter document number"
+              />
+              <!-- <span class="text-sm font-light text-red-500" v-if="v$.identityForm.document.document_id.$dirty && v$.identityForm.document.document_id.required.$invalid">
+            This field is required
+          </span> -->
+            </div>
+            <span class="material-icons text-red-500 my-auto">delete_forever</span>
+          </section>
+        </div>
+        <button
+          class="
+            ring-1 ring-gray-200
+            font-medium
+            text-sm
+            mb-
+            flex flex-row
+            mr-4
+            px-2
+            py-3
+            rounded-lg
+            mt-6
+          "
+          @click="addNewStakeHolder"
+        >
+          <span class="material-icons">add</span>
+          Add new shareholder
+        </button>
+      </div>
+      <div v-if="step === 3" class="h-full p-10 bg-white">HEere is three</div>
     </div>
   </page-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import PageLayout from "@/components/layout/PageLayout.vue";
+import PageLayout from '@/components/layout/PageLayout.vue';
 import AppModal from '@/components/Modals/AppModal.vue';
+import ImageUpload from '@/components/ImageUpload.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     PageLayout,
-    AppModal
+    ImageUpload
+  },
+  data() {
+    return {
+      step: 2,
+      stakeholders: [
+        {
+          name: '',
+          percent: 0
+        }
+      ]
+    };
+  },
+  methods: {
+    addNewStakeHolder() {
+      console.log('I am here');
+      this.stakeholders.push({
+        name: '',
+        percent: 0
+      });
+    }
   }
-})
+});
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/views/app/vehicles/steps/VehicleImages.vue
+++ src/views/app/vehicles/steps/VehicleImages.vue
@@ -6,7 +6,12 @@
         <label class="text-xs font-medium text-grays-black-5"
           >Upload Vehicle interior document (pdf, jpg, png)</label
         >
-        <MultipleImageUpload :imageUrls="interiorImages" @filesSelected="selectFile($event, 'interior')" @fileRemoved="removeFiles('interior')" class="pt-3"/>
+        <MultipleImageUpload
+          :imageUrls="interiorImages"
+          @filesSelected="selectFile($event, 'interior')"
+          @fileRemoved="removeFiles('interior')"
+          class="pt-3"
+        />
       </div>
 
       <div class="">
@@ -14,14 +19,37 @@
         <label class="text-xs font-medium text-grays-black-5"
           >Upload Vehicle Exterior document (pdf, jpg, png)</label
         >
-        <MultipleImageUpload :imageUrls="exteriorImages" @filesSelected="selectFile($event, 'exterior')" @fileRemoved="removeFiles('exterior')" class="pt-3"/>
+        <MultipleImageUpload
+          :imageUrls="exteriorImages"
+          @filesSelected="selectFile($event, 'exterior')"
+          @fileRemoved="removeFiles('exterior')"
+          class="pt-3"
+        />
       </div>
-
     </main>
 
     <div class="flex justify-end items-center space-x-5 pt-5">
-     <button type="button" class="text-black text-sm bg-gray-300 px-6 py-3 font-medium rounded-md" @click="viewVehicleDetails(getVehicleFormData.id)"> Skip </button>
-     <button type="button" class="text-black text-sm bg-sh-green-500 px-6 py-3 font-medium rounded-md" @click="submitFinalForm"> Submit </button>
+      <button
+        type="button"
+        class="text-black text-sm bg-gray-300 px-6 py-3 font-medium rounded-md"
+        @click="viewVehicleDetails(getVehicleFormData.id)"
+      >
+        Skip
+      </button>
+      <button
+        type="button"
+        class="
+          text-black text-sm
+          bg-sh-green-500
+          px-6
+          py-3
+          font-medium
+          rounded-md
+        "
+        @click="submitFinalForm"
+      >
+        Submit
+      </button>
     </div>
   </form>
 </template>
@@ -42,26 +70,31 @@ export default defineComponent({
     })
   },
   methods: {
-    async selectFile ($event: File, type: string) {
+    async selectFile($event: File, type: string) {
       const multipleDocumentObjects = $event;
-      const multipleDocumentList = this.convertImageObjectToList(multipleDocumentObjects);
+      const multipleDocumentList = this.convertImageObjectToList(
+        multipleDocumentObjects
+      );
       this.uploadTos3andGetDocumentUrlList(multipleDocumentList, type);
     },
-    removeFiles (type: any) {
+    removeFiles(type: any) {
       if (type === 'interior') {
-        this.interiorImages = []
+        this.interiorImages = [];
       }
       if (type === 'exterior') {
-        this.exteriorImages = []
+        this.exteriorImages = [];
       }
     },
-    uploadTos3andGetDocumentUrlList (multipleImages: Array<File>, type: string) {
+    uploadTos3andGetDocumentUrlList(multipleImages: Array<File>, type: string) {
       this.uploadingFile = true;
-      multipleImages.forEach(async file => {
+      multipleImages.forEach(async (file) => {
         try {
           const formData = new FormData();
           formData.append('file', file);
-          const response = await this.$axios.post(`/v1/upload/identity/files`, formData);
+          const response = await this.$axios.post(
+            `/v1/upload/identity/files`,
+            formData
+          );
           if (response.data?.files?.length) {
             if (type === 'interior') {
               this.interiorImages.push(response.data.files[0].Location);
@@ -71,36 +104,44 @@ export default defineComponent({
             }
           }
         } catch (error) {
-          this.$toast.warning("An error occured while uploading your file, please try again")
+          this.$toast.warning(
+            'An error occured while uploading your file, please try again'
+          );
         }
       });
       this.uploadingFile = false;
-      this.$toast.success(`${type} images uploaded`)
+      this.$toast.success(`${type} images uploaded`);
     },
-    convertImageObjectToList (imageObject: any) {
+    convertImageObjectToList(imageObject: any) {
       const imageList = [];
       for (const key in imageObject) {
         if (imageObject[key].size) {
-          imageList.push(imageObject[key])
+          imageList.push(imageObject[key]);
         }
       }
       return imageList;
     },
-    viewVehicleDetails (id : number) {
+    viewVehicleDetails(id: number) {
       this.$router.push({
         name: 'vehicle.detail.info',
-        params: {vehicleId: id}
+        params: { vehicleId: id }
       });
     },
-    async submitFinalForm () {
+    async submitFinalForm() {
       this.submittingFinalForm = true;
       try {
-        await this.$axios.post(`/v1/partners/${this.partnerContext.partner.id}/vehicle/${this.getVehicleFormData.id}/vehicle-images`, {
-          uploads: [...this.interiorImages]
-        });
-        await this.$axios.post(`/v1/partners/${this.partnerContext.partner.id}/vehicle/${this.getVehicleFormData.id}/vehicle-images`, {
-          uploads: [...this.exteriorImages]
-        });
+        await this.$axios.post(
+          `/v1/partners/${this.partnerContext.partner.id}/vehicle/${this.getVehicleFormData.id}/vehicle-images`,
+          {
+            uploads: [...this.interiorImages]
+          }
+        );
+        await this.$axios.post(
+          `/v1/partners/${this.partnerContext.partner.id}/vehicle/${this.getVehicleFormData.id}/vehicle-images`,
+          {
+            uploads: [...this.exteriorImages]
+          }
+        );
         this.viewVehicleDetails(this.getVehicleFormData.id);
       } catch (error) {
         const errorMessage = extractErrorMessage(
@@ -108,27 +149,26 @@ export default defineComponent({
           null,
           'Oops! An error occurred, please try again.'
         );
-        if (errorMessage === "\"uploads\" must contain at least 1 items") {
-          this.$toast.error("All Vehicle Images must be uploaded")
+        if (errorMessage === '"uploads" must contain at least 1 items') {
+          this.$toast.error('All Vehicle Images must be uploaded');
         } else {
           this.$toast.error(errorMessage);
         }
       } finally {
-        this.submittingFinalForm = false
+        this.submittingFinalForm = false;
       }
-    },
+    }
   },
-  data () {
+  data() {
     return {
       submittingFinalForm: false,
       uploadingFile: false,
       interiorImages: [] as Array<any>,
       exteriorImages: [] as Array<any>
-    }
+    };
   }
-})
+});
 </script>
 
 <style>
-
 </style>
