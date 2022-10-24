07770af52fedf9a87e1ddcb5188479b8f4c79d85
Okafor Akachukwu <akachukwu@shuttlers.ng>
2022-10-24 16:10:06 +0100

    resolved issues with ui tabs

--- src/views/app/Sidebar.vue
+++ src/views/app/Sidebar.vue
@@ -55,7 +55,7 @@
         </div>
         <div class="side-bar-item">
           <div v-for="(menu, index) in group.menus" :key="index" class="item">
-            <div
+            <router-link
               class="
                 h-12
                 rounded-lg
@@ -68,40 +68,36 @@
                 duration-200
               "
               :class="{
-                'bg-black': menu.selected,
                 'justify-center': isSidebarFolded,
                 'justify-between pl-5': !isSidebarFolded
               }"
-              @click="selectThisSection(groupIndex, index)"
+              :to="{name: menu.routeName}"
             >
-              <div class="flex flex-row items-center">
+              <div class="flex flex-row items-center text-gray-500">
                 <span
-                  class="material-icons"
+                  class="material-icons sidemenu-icon"
                   :class="{
-                    'text-white': menu.selected,
-                    'text-gray-500': !menu.selected,
                     'mr-3': !isSidebarFolded
                   }"
                   >{{ menu.icon }}</span
                 >
                 <span
-                  class="font-sm"
-                  :class="menu.selected ? 'text-white' : 'text-gray-500'"
+                  class="font-sm sidemenu-icon-label"
                 >
                   {{ isSidebarFolded ? '' : menu.title }}
                 </span>
               </div>
               <div>
                 <div
-                  class="h-5 w-1"
+                  class="h-5 w-1 sidebar-active-status"
                   :class="
-                    menu.selected && !isSidebarFolded
-                      ? 'bg-sh-green-500'
+                   !isSidebarFolded
+                      ? ''
                       : 'hidden'
                   "
                 ></div>
               </div>
-            </div>
+            </router-link>
           </div>
         </div>
       </div>
@@ -137,6 +133,7 @@
   </div>
 </template>
 <script lang="ts">
+// @click.prevent="selectThisSection(groupIndex, index)"
 import { defineComponent } from 'vue';
 import { mapGetters } from 'vuex';
 export default defineComponent({
@@ -150,17 +147,17 @@ export default defineComponent({
         {
           sectionTitle: 'GENERAL',
           menus: [
-            { title: 'Dashboard', icon: 'home', selected: true },
-            { title: 'Vehicles', icon: 'directions_car', selected: false },
-            { title: 'Drivers', icon: 'group', selected: false },
-            { title: 'Trips', icon: 'route', selected: false }
+            { title: 'Dashboard', routeName: 'dashboard', icon: 'home', selected: true },
+            { title: 'Vehicles', routeName: 'vehicles.list', icon: 'directions_car', selected: false },
+            { title: 'Drivers', routeName: 'drivers.list', icon: 'group', selected: false },
+            { title: 'Trips', routeName: 'trips.list', icon: 'route', selected: false }
             // {title: 'Earnings', icon: 'payments', selected: false}
           ]
         },
         {
           sectionTitle: 'SYSTEM',
           menus: [
-            {title: 'Settings', icon: 'settings', selected: false}
+            {title: 'Settings', routeName: 'settings.edit.partner', icon: 'settings', selected: false}
           ]
         }
       ]
@@ -208,3 +205,16 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped lang="scss">
+  .router-link-active {
+    @apply bg-black
+  }
+
+  .router-link-active .sidemenu-icon, .router-link-active .sidemenu-icon-label {
+      @apply text-white
+    }
+    .router-link-active .sidebar-active-status {
+      @apply bg-sh-green-500;
+    }
+</style>
