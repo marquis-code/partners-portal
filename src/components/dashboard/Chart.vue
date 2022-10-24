157ab85bcd4a07cd9987de9624397b200cb8895b
marquis-code <abahmarquis@gmail.com>
2022-10-24 03:22:26 +0100

    setup charts

---
+++ src/components/dashboard/Chart.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="app">
+    <!-- <button type="button" @click="changeData">Change data</button> -->
+
+    <apexcharts
+      type="bar"
+      :options="chartOptions"
+      :series="series"
+    ></apexcharts>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts';
+// import ApexCharts from 'apexcharts';
+
+export default {
+  name: 'Chart',
+  components: {
+    apexcharts: VueApexCharts
+  },
+  data: function () {
+    return {
+      secondData: [
+        {
+          name: 'Series1',
+          data: [51, 42, 109, 38, 47]
+        },
+        {
+          name: 'Series2',
+          data: [56, 90, 12, 32, 44]
+        },
+        {
+          name: 'Series3',
+          data: [22, 43, 21, 100, 56]
+        }
+      ],
+      Secondcategories: ['2015', '2016', '2017', '2018', '2019']
+    };
+  },
+
+  // methods: {
+  //   async changeData() {
+  //     ApexCharts.exec('chart1', 'updateOptions', {
+  //       xaxis: {
+  //         categories: this.Secondcategories
+  //       }
+  //     });
+
+  //     ApexCharts.exec('chart1', 'updateSeries', [
+  //       {
+  //         data: this.secondData
+  //       }
+  //     ]);
+  //   }
+  // },
+
+  computed: {
+    series: function () {
+      return [
+        {
+          name: 'series1',
+          data: [31, 40, 28, 51, 42, 109, 100, 38, 47, 58, 77, 84]
+        },
+        {
+          name: 'series2',
+          data: [100, 87, 12, 31, 42, 64, 55, 20, 97, 28, 37, 104]
+        },
+        {
+          name: 'Series3',
+          data: [44, 55, 41, 64, 22, 43, 21, 80, 22, 100, 56, 70]
+        }
+      ];
+    },
+    chartOptions: function () {
+      return {
+        dataLabels: {
+          enabled: false
+        },
+        chart: {
+          id: 'chart1',
+          toolbar: {
+            show: false
+          }
+        },
+        stroke: {
+          width: 0.2,
+          colors: ['#74fb00'],
+          curve: 'smooth'
+        },
+
+        dropShadow: {
+          enabled: true,
+          top: 0,
+          left: 0,
+          blur: 3,
+          opacity: 0.5
+        },
+
+        theme: {
+          monochrome: {
+            enabled: true,
+            shadeTo: 'light',
+            shadeIntensity: 0.65
+          }
+        },
+
+        grid: {
+          yaxis: {
+            lines: {
+              show: false,
+              offsetX: 0,
+              offsetY: 0
+            }
+          }
+        },
+
+        xaxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+          ]
+        }
+      };
+    }
+  }
+};
+</script>
---
+++ src/components/dashboard/Vehicles.vue
@@ -0,0 +1,28 @@
+<template>
+  <main
+    class="
+      rounded-md
+      ring-1 ring-gray-200
+      bg-grays-white
+      shadow-sm
+      p-6
+      space-y-5
+    "
+  >
+    <img src="@/assets/images/vehiclesIcon.svg" />
+    <p class="text-grays-black-4">Total Vehicles</p>
+    <h1 class="text-sh-grey-900 font-bold">1</h1>
+    <div class="bg-grays-black-7 w-full h-0.5"></div>
+    <div class="flex items-center space-x-3">
+      <img src="@/assets/images/addDriverIcon.svg" />
+      <span class="text-sh-purple-700 text-sm">Add Vehicle</span>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
