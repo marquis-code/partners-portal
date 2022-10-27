153bcdeb81626c55d71158d7aa4fcead8bbd05ee
marquis-code <abahmarquis@gmail.com>
2022-10-27 10:12:11 +0100

    setup bounds

--- src/components/map/GoogleMaps.vue
+++ src/components/map/GoogleMaps.vue
@@ -1,23 +1,25 @@
 <template>
   <GoogleMap
+    ref="googleMapInstance"
     :api-key="mapAPIKey"
     style="width: 100vw; height: 100vh"
     :center="centerLocation"
     :zoom="6"
   >
-    <Marker :options="{ position: startLocation }"/>
+    <Marker :options="{ position: startLocation }" />
     <Marker :options="{ position: endLocation }" />
     <Polyline :options="flightPath" />
   </GoogleMap>
 </template>
 
 <script lang="ts">
+/// <reference types="google.maps" />
 import { defineComponent } from 'vue';
 import { GoogleMap, Marker, Polyline } from 'vue3-google-map';
 
 interface LocationType {
-  lat: number,
-  lng: number
+  lat: number;
+  lng: number;
 }
 export default defineComponent({
   name: 'GoogleMaps',
@@ -36,7 +38,7 @@ export default defineComponent({
       type: Object as () => LocationType
     }
   },
-  data () {
+  data() {
     return {
       center: { lat: 3.64003, lng: 6.46767 },
       flightPath: {
@@ -44,14 +46,41 @@ export default defineComponent({
         geodesic: true,
         strokeColor: '#0DAC5C',
         strokeOpacity: 1.0,
-        strokeWeight: 4,
+        strokeWeight: 4
       },
-      mapAPIKey: process.env.VUE_APP_GOOGLE_API_KEY || '' as string
-    }
+      mapAPIKey: process.env.VUE_APP_GOOGLE_API_KEY || ('' as string)
+    };
   },
-  created () {
+  created() {
     this.flightPath.path = this.routeLine || [];
-    console.log(this.mapAPIKey)
+    console.log(this.mapAPIKey);
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.zoomToBounds();
+    });
+  },
+  methods: {
+    zoomToBounds(attempts = 0) {
+      if (attempts > 20) return;
+
+      if (!(this.$refs.googleMapInstance as any)?.ready) {
+        setTimeout(() => {
+          this.zoomToBounds(attempts++);
+        }, 500);
+      }
+      let bounds = new window.google.maps.LatLngBounds();
+      this.routeLine?.forEach((entry) => {
+        bounds = bounds.extend(entry);
+      });
+
+      console.log(
+        this.$refs.googleMapInstance,
+        bounds.getNorthEast(),
+        bounds.getSouthWest()
+      );
+      (this.$refs.googleMapInstance as any)?.map?.fitBounds(bounds, 250);
+    }
   }
 });
 </script>
