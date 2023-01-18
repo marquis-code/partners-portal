46f37f4745a4bb79722e5504fe2385008cc6a523
encodedBicoding <dominicisioma000@gmail.com>
2023-01-18 20:44:56 +0300

    updates on pr

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -8,7 +8,7 @@ import {Token} from "@/models/login-response.model";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const config = {
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  baseURL: 'https://api.test.shuttlers.africa/',
   timeout: 60 * 1000 // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
