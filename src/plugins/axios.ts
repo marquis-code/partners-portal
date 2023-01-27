1a99ffff5492b1a0d2cf8e680ba37678dcea12c4
encodedBicoding <dominicisioma000@gmail.com>
2023-01-27 11:58:54 +0300

    updates config

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -7,16 +7,16 @@ import {Token} from "@/models/login-response.model";
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-// const config = {
-//   baseURL: process.env.VUE_APP_API_BASE_URL,
-//   timeout: 60 * 1000 // Timeout
-//   // withCredentials: true, // Check cross-site Access-Control
-// };
 const config = {
-  baseURL: 'https://api.test.shuttlers.africa/',
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   timeout: 60 * 1000 // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
+// const config = {
+//   baseURL: 'https://api.test.shuttlers.africa/',
+//   timeout: 60 * 1000 // Timeout
+//   // withCredentials: true, // Check cross-site Access-Control
+// };
 export const axiosInstance = axios.create(config);
 
 /* _axios.interceptors.request.use(
