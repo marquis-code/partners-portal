c5cf1a79edf54af13f1856ec0785ab826f7705ff
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-05-11 21:08:56 +0100

    fixing the build error in github action

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -10,3 +10,12 @@ declare module 'vue/types/vue' {
     $axios: AxiosStatic
   }
 }
+
+import { RouteLocationNormalizedLoaded, Router } from 'vue-router'
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $router: Router
+    $route: RouteLocationNormalizedLoaded
+  }
+}
