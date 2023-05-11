5a8c6ccba75b717d1a83e90af63e32ee5eb5b580
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-05-11 12:23:52 +0100

    done with payslip

--- src/views/app/earnings/views/Payslip.vue
+++ src/views/app/earnings/views/Payslip.vue
@@ -22,7 +22,8 @@
 </template>
 <div>
   <div class="flex flex-col gap-2 bg-white rounded p-2">
-    <div class="flex justify-end py-2">
+    <div class="flex justify-end gap-4 items-center py-2">
+      <p>Filter by year:</p>
       <select v-model="filterBy" class="border rounded p-2">
         <option value="">No filter</option>
         <option v-for="n in filterData" :key="n" :value="n">{{ n }}</option>
@@ -34,10 +35,23 @@
       :items="filteredTableData"
       :fields="headers"
       @rowClicked="(e:any) => viewPaymentDetails(e)"
+      @page-change="loadPageChange"
     >
 
+      <template v-slot:total_amount="{item}">
+        {{ Intl.NumberFormat('en-US').format(item.amount + item.totalDeduction)}}
+      </template>
+      <template v-slot:totalDeduction="{item}">
+        {{ Intl.NumberFormat('en-US').format(item.totalDeduction)}}
+      </template>
+      <template v-slot:amount="{item}">
+        {{ Intl.NumberFormat('en-US').format(item.amount)}}
+      </template>
       <template v-slot:payoutDate="{item}">
         {{ moment(item.payoutDate).format('MMM YYYY') }}
+      </template>
+      <template v-slot:status="{item}">
+        {{ item.status == '2' ? 'Settled' : item.status == '1' ? 'Pending Settlement' : item.status == '0' ? 'Pending Payout' : 'Failed' }}
       </template>
 
     </app-table>
@@ -55,12 +69,15 @@ import { useStore } from 'vuex';
 import {axiosInstance as axios} from '@/plugins/axios';
 import AppTable from '@/components/AppTable.vue';
 import moment from 'moment';
+import { formatNaira } from '@/composables/utils';
 
 const store = useStore()
 const headers = [
   { label: 'Payout Month', key: 'payoutDate' },
-  { label: 'Amount', key: 'amount' },
-  { label: 'Deduction', key: 'totalDeduction' }
+  { label: 'Total Amount', key: 'total_amount' },
+  { label: 'Total Deduction', key: 'totalDeduction' },
+  { label: 'Net Income', key: 'amount' },
+  { label: 'Status', key: 'status' }
 ]
 const filterBy = ref('')
 const filterData = ['2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015']
@@ -81,12 +98,11 @@ const partnerContext:any = computed(() => store.getters['auth/activeContext'])
 const filteredTableData = computed(() => {
   return tableData.value.filter(el => el.payoutDate.includes(filterBy.value))
 })
-const getEarnings = async () => {
+const getEarnings = async (page = 1) => {
   loading.value = true
   try {
     const res = await axios.get(
-      // `/cost-revenue/v1/partners/${partnerContext.value.partner.account_sid}/earnings-summary`
-      `/cost-revenue/v1/earnings?status=pending-payout,pending-settlement,failed&partnerId=${partnerContext.value.partner.account_sid}`
+      `/cost-revenue/v1/earnings?isApproved=false&partnerId=${partnerContext.value.partner.account_sid}&status=pending-payout,pending-settlement,failed,settled&page=${page}&limit=20`
     );
     tableData.value = res.data.result
     console.log(res)
@@ -95,6 +111,10 @@ const getEarnings = async () => {
   } finally {
     loading.value = false
   }
+}
+
+const loadPageChange = (page:number) => {
+  getEarnings(page)
 }
 getEarnings()
 
--- src/views/app/earnings/views/Payslip_details.vue
+++ src/views/app/earnings/views/Payslip_details.vue
@@ -13,26 +13,29 @@
       </div>
     </template>
     <div class="w-full flex flex-col gap-8">
-      <div class="w-[90%] max-w-[500px] mx-auto bg-white rounded border flex flex-col gap-4 p-4 md:p-6">
+      <div v-if="total_loading" class="w-[90%] h-[200px] max-w-[500px] mx-auto bg-white rounded border flex items-center justify-center">
+        <spinner />
+      </div>
+      <div v-else class="w-[90%] max-w-[500px] mx-auto bg-white rounded border flex flex-col gap-4 p-4 md:p-6">
         <div class="border-b text-gray-600 font-medium py-1">
           Payout
         </div>
         <div class="flex flex-col gap-1 mx-auto w-fit items-center">
-          <h1 class="text-4xl font-bold">₦ 1000</h1>
+          <h1 class="text-4xl font-bold">₦ {{ Intl.NumberFormat('en-US').format(totalData.netRevenue) }}</h1>
           <h4 class="font-medium text-gray-600">PARTNER'S PAYOUT</h4>
         </div>
         <div class="flex flex-col">
           <div class="border-b py-2 flex items-center justify-between">
             <p class="text-gray-500">Total Amount:</p>
-            <p class="text-black font-bold">₦ 1000</p>
+            <p class="text-black font-bold">₦ {{ Intl.NumberFormat('en-US').format(totalData.totalRevenue) }}</p>
           </div>
           <div class="border-b py-2 flex items-center justify-between">
             <p class="text-gray-500">Deductions:</p>
-            <p class="text-black font-bold">₦ 1000</p>
+            <p class="text-black font-bold">₦ {{ Intl.NumberFormat('en-US').format(totalData.totalDeduction) }}</p>
           </div>
           <div class="py-2 flex items-center justify-between">
             <p class="text-gray-500">Net Income:</p>
-            <p class="text-black font-bold">₦ 1000</p>
+            <p class="text-black font-bold">₦ {{ Intl.NumberFormat('en-US').format(totalData.netRevenue) }}</p>
           </div>
         </div>
       </div>
@@ -43,12 +46,25 @@
             :error-loading="loading"
             :items="tableData"
             :fields="headers"
+            @page-change="loadPageChange"
           >
+            <template v-slot:tripStartTime="{ item }">
+              {{ moment(item.tripStartTime).format('Do MMM YYYY') }}
+            </template>
             <template v-slot:route="{ item }">
               <trip-history
-              :pickup="item?.route?.pickup"
-              :destination="item?.route?.destination"
+              :pickup="item?.metadata?.pickup"
+              :destination="item?.metadata?.dropoff"
               ></trip-history>
+            </template>
+            <template v-slot:finalPartnersRevenue="{ item }">
+              {{ Intl.NumberFormat('en-US').format(item.finalPartnersRevenue)}}
+            </template>
+            <template v-slot:totalDeductedAmount="{ item }">
+              {{ Intl.NumberFormat('en-US').format(item.totalDeductedAmount) }}
+            </template>
+            <template v-slot:netIncome="{ item }">
+              {{ Intl.NumberFormat('en-US').format(item.finalPartnersRevenue - item.totalDeductedAmount) }}
             </template>
           </app-table>
       </div>
@@ -57,7 +73,7 @@
 </main>
 </template>
 <script setup lang="ts">
-import { ref, Ref, onMounted, computed } from 'vue'
+import { ref, Ref, computed } from 'vue'
 import PageLayout from '@/components/layout/PageLayout.vue';
 import AppTable from '@/components/AppTable.vue';
 import TripHistory from '@/components/TripHistory.vue';
@@ -65,29 +81,31 @@ import { useStore } from 'vuex';
 import moment from 'moment';
 import {axiosInstance as axios} from '@/plugins/axios';
 import { useRoute } from 'vue-router';
+import spinner from '@/components/loader/spinner.vue';
 
 const route = useRoute()
 const id = route.params.id as string
 const store = useStore()
 const headers = [
-  { label: 'Trip date', key: 'payoutDate' },
-  { label: 'Route', key: 'amount' },
-  { label: 'Route Code', key: 'totalDeduction' },
-  { label: 'Amount Earned', key: 'payoutDate' },
-  { label: 'Deductions', key: 'amount' },
-  { label: 'Net Income', key: 'totalDeduction' },
-  { label: 'Status', key: 'status' }
+  { label: 'Trip date', key: 'tripStartTime' },
+  { label: 'Route', key: 'route' },
+  { label: 'Route Code', key: 'metadata.routeCode' },
+  { label: 'Amount Earned', key: 'finalPartnersRevenue' },
+  { label: 'Deductions', key: 'totalDeductedAmount' },
+  { label: 'Net Income', key: 'netIncome' }
 ]
+const totalData = ref({}) as Ref<any>
 const tableData = ref([]) as Ref<any[]>
 const loading = ref(false)
+const total_loading = ref(false)
 
 const partnerContext:any = computed(() => store.getters['auth/activeContext'])
 
-const getData = async () => {
+const getData = async (page = 1) => {
   loading.value = true
   try {
     const res = await axios.get(
-      `/cost-revenue/v1/earnings?status=pending-payout,pending-settlement,failed&partnerId=${partnerContext.value.partner.account_sid}`
+      `/cost-revenue/v1/partners/${partnerContext.value.partner.account_sid}/revenues?page=${page}&perPage=10&earningId=${id}`
     );
     tableData.value = res.data.result
     console.log(res)
@@ -97,4 +115,26 @@ const getData = async () => {
     loading.value = false
   }
 }
+
+const getTotalEarnings = async () => {
+  total_loading.value = true
+  try {
+    const res = await axios.get(
+      `/cost-revenue/v1/earnings/${id}`
+    );
+    console.log(res)
+    totalData.value = res.data
+  } catch (err) {
+    console.log(err)
+  } finally {
+    total_loading.value = false
+  }
+}
+
+const loadPageChange = (page:number) => {
+  getData(page)
+}
+
+getData()
+getTotalEarnings()
 </script>
