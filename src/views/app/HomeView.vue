21554c73834cf498b978ccc5b9a9803eb12803f2
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-06-22 18:13:45 +0100

    adjusted sidebar for mobile view

--- src/views/app/HomeView.vue
+++ src/views/app/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home flex flex-row">
-    <sidebar @sideBarNavigationClicked="changeNavigation"/>
+    <sidebar class="hidden md:flex" @sideBarNavigationClicked="changeNavigation"/>
     <div class="w-full pb-[80px]">
       <router-view/>
     </div>
+</div>
+<div v-if="active" class="fixed top-0 left-0 w-full h-full bg-white z-20 flex gap-4 justify-between">
+  <sidebar class="max-w-[230px]" @sideBarNavigationClicked="changeNavigation"/>
+  <div class="flex-grow bg-[#2b29291b]" @click="closeSidebar"></div>
 </div>
 </template>
 
@@ -29,14 +33,22 @@ export default defineComponent({
 </script> -->
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import Sidebar from "./Sidebar.vue";
 import router from "@/router";
+import { useSidebar } from "@/composables/sidebar";
+import { useRoute } from "vue-router";
 
+const route = useRoute()
+const { active, closeSidebar } = useSidebar()
 const activeSection = ref("View All")
 
 const changeNavigation = (_activeSection: string) => {
   activeSection.value = _activeSection;
   router.push(`/${_activeSection.toLowerCase()}`);
 }
+
+watch(route, () => {
+  closeSidebar()
+})
 </script>
