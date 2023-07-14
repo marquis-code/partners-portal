8029f78063eeaee04e3516c9f36fe49314078196
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-07-14 19:13:34 +0100

    update on faq page

--- src/views/faq.vue
+++ src/views/faq.vue
@@ -35,6 +35,19 @@ import { ref } from 'vue'
 import router from '@/router'
 const items = ref([
   {
+    title: 'How do I become a verified bus partner?',
+    content: 'To become a verified bus partner, it is important to follow these steps:',
+    expanded: false,
+    steps: [
+      'Complete KYC (Know Your Customer) Process: Ensure that you complete the KYC process, which includes providing necessary documentation for identity verification and address verification.',
+      'Verify Your Identity: Submit the required identification documents to verify your identity. This typically involves providing government-issued identification such as NIN, International passport, driver\'s license or passport',
+      'Address Verification: Provide proof of your address by submitting relevant documents such as utility bills, bank statements, or lease agreements. This helps establish your business location.',
+      'Add Drivers and Vehicles: Include information about your drivers and their respective vehicles. Provide necessary details such as driver\'s licenses, vehicle registration documents, and insurance information.',
+      'Set Up a Settlement Account: Add your settlement account details where you would like to receive payments for your services. This typically involves providing your bank account information for direct deposits.',
+      'By following these steps and completing the necessary requirements, you can become a verified bus partner. It is essential to comply with the partner program\'s guidelines and regulations to ensure a smooth verification process.'
+    ]
+  },
+  {
     title: 'How do I link my vehicle and pilots on the dashboard?',
     content: 'To link your vehicles to your account on the dashboard, navigate to the "Vehicles" section and click on it. Add all your buses by following the instructions provided. Similarly, to link your drivers, locate the "Drivers" feature and click on it. From there, you can easily add your drivers to your account. Additionally, through this feature, you have the option to remove any drivers who are no longer working for you.',
     expanded: false,
@@ -57,20 +70,7 @@ const items = ref([
     content: 'In case of a bus breakdown, please inform our support team immediately. We will work with you to resolve the situation and provide assistance to affected passengers.',
     expanded: false,
     steps: []
-  },
-  {
-    title: 'How do I become a verified bus partner?',
-    content: 'To become a verified bus partner, it is important to follow these steps:',
-    expanded: false,
-    steps: [
-      'Complete KYC (Know Your Customer) Process: Ensure that you complete the KYC process, which includes providing necessary documentation for identity verification and address verification.',
-      'Verify Your Identity: Submit the required identification documents to verify your identity. This typically involves providing government-issued identification such as NIN, International passport, driver\'s license or passport',
-      'Address Verification: Provide proof of your address by submitting relevant documents such as utility bills, bank statements, or lease agreements. This helps establish your business location.',
-      'Add Drivers and Vehicles: Include information about your drivers and their respective vehicles. Provide necessary details such as driver\'s licenses, vehicle registration documents, and insurance information.',
-      'Set Up a Settlement Account: Add your settlement account details where you would like to receive payments for your services. This typically involves providing your bank account information for direct deposits.',
-      'By following these steps and completing the necessary requirements, you can become a verified bus partner. It is essential to comply with the partner program\'s guidelines and regulations to ensure a smooth verification process.'
-    ]
-  },
+  }
 ])
 
 const toggleAccordion = (index: number) => {
