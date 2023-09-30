ed80491b56167e62315c0f87cea418a4efb273c4
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-09-30 15:20:50 +0100

    lot of fixes on portal

--- src/views/app/settings/EditPartnerInfo.vue
+++ src/views/app/settings/EditPartnerInfo.vue
@@ -3,7 +3,7 @@
       <spinner></spinner>
     </div>
     <main class="md:w-9/12 p-5 lg:p-14 bg-white ring-1 ring-gray-100">
-      <div class="flex justify-center items-center flex-col space-y-2 pb-5">
+      <!-- <div class="flex justify-center items-center flex-col space-y-2 pb-5">
         <img
           v-if="profilePreview && !uploadingProfile"
           class="h-14 w-14 rounded-full object-cover"
@@ -29,7 +29,7 @@
             uploadingProfile ? 'Uploading...' : 'Click to upload image'
           }}</label
         >
-      </div>
+      </div> -->
       <div>
         <p class="text-sm text-gray-400 pb-3">Partners Information</p>
         <form class="space-y-3 lg:space-y-7">
@@ -681,7 +681,7 @@ const getDriverData:any = computed(() => store.getters['driver/getDriverData'])
 const driverData:any = computed(() => store.getters['driver/getDriverData'])
 
 const setCurrentDetails = () => {
-  form.value.residential_address = partnerContext.value.onboardingState.address.address.slice(1, partnerContext.value.onboardingState.address.address.length - 1);
+  form.value.residential_address = partnerContext.value.onboardingState.address.address;
   form.value.fname = userSessionData.value.user.fname;
   form.value.lname = userSessionData.value.user.lname;
   form.value.phone = userSessionData.value.user.phone;
