35ef9f7eb5af67a6c24065325840f80ee8b11695
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-10-27 13:16:25 +0100

    update on login button

--- src/views/auth/login/EmailLogin.vue
+++ src/views/auth/login/EmailLogin.vue
@@ -71,9 +71,9 @@
         "
         class="flex justify-center items-center w-full p-3 rounded-md border-none outline-none text-sm mt-8"
       >
-        {{ processing ? 'Loading' : 'Login' }}
-        <spinner v-if="processing" class="w-5 ml-2 mx-0"></spinner>
-        <img v-if="!processing" class="ml-2" src="@/assets/images/arrow.svg" />
+        <span>{{ processing ? 'Loading' : 'Login' }}</span>
+        <spinner v-if="processing" class="ml-2 w-fit mx-0"></spinner>
+        <img v-else class="ml-2" src="@/assets/images/arrow.svg" />
       </button>
 
       <div
